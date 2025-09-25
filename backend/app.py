6f5d72b35eaa7100d9cb20e31f589e2ea4b0b6b1
Vinayak-Jolly <vinayakjolly7@gmail.com>
2025-09-26 00:16:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,12 @@ import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=[""], allow_methods=[""], allow_headers=["*"])
+
 
 app = Flask(__name__)
 
